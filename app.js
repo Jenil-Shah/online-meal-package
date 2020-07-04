02c2cb0e180aa0843f403ad254add4c8d3977b69
Jenil-Shah <jenilshah757@gmail.com>
2020-07-04 17:14:09 -0400

    Registration and Login forms validation

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const meal = require("./models/homeTop");
 const topMealDB = require("./models/homeTop");
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
@@ -46,6 +49,112 @@ app.get("/login",(req,res) => {
         h1_quote:"Visit our Meal Packages",
         h3_quote:"Enjoy the Variety!!!"
     });
+});
+
+app.get("/dashBoard",(req,res) => {
+    res.render("dashBoard",{
+        title:"Dash Board"
+    });
+});
+
+app.post("/registration",(req,res) => {
+
+    const errors = [];
+    const values =
+        {
+            firstName : req.body.Fname,
+            lastName : req.body.Lname,
+            email : req.body.Email,
+            password : req.body.Password
+        }
+
+
+    if(values.firstName=="")
+    {
+        errors.push({first:"*You must enter first name"});
+    }
+
+    if(values.lastName=="")
+    {
+        errors.push({last:"*You must enter last name"});
+    }
+
+    if(values.email=="")
+    {
+        errors.push({email:"*You must enter email"});
+    }
+
+    if(values.password=="")
+    {
+        errors.push({password:"*You must enter password"});
+    }
+
+    if(values.password.length < 6 || values.password.length > 12)
+    {
+        errors.push({password:"*must enter password between 6 and 12 characters"});
+    }
+    
+    const regex = /[^A-Za-z0-9]/g;
+
+    if(values.password.match(regex))
+    {
+        errors.push({password:"*password should only contain alphabets and numbers"});
+    }
+
+
+    if(errors.length > 0)
+    {
+        res.render("registration",{
+            title:"Sign up",
+            h1_quote:"Create Your Account",
+            h3_quote:"Get chance to win our Gift Card",
+            errorMessages:errors,
+            field:values
+        });
+    }
+
+    else
+    {
+        res.redirect("/dashBoard");
+    }
+
+});
+
+app.post("/login",(req,res) => {
+
+    const errors = [];
+    const values =
+    {
+        email : req.body.EmailLog,
+        password : req.body.PasswordLog
+    }
+
+    if(values.email=="")
+    {
+        errors.push({email:"*you must enter email"});
+    }
+
+    if(values.password=="")
+    {
+        errors.push({password:"*you must enter password"});
+    }
+
+    if(errors.length > 0)
+    {
+        res.render("login",{
+            title:"Log In",
+            h1_quote:"Visit our Meal Packages",
+            h3_quote:"Enjoy the Variety!!!",
+            errorMessages:errors,
+            field:values
+        });
+    }
+
+    else
+    {
+        res.redirect("/");
+    }
+
 });
 
 const PORT = 3000;
